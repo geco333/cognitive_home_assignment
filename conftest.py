a4a1bd94d3c0d5416a89d3df5bb48e374b35c015
unknown <geco333@gmail.com>
2024-07-19 08:56:33 +0300

    Added logging to file

--- conftest.py
+++ conftest.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import pathlib
 import uuid
 
 # Setup logging to a local file named using a random uuid4 hash.
@@ -6,7 +8,12 @@ logging.basicConfig(force=True)
 
 root_logger = logging.getLogger()
 
-file_handler = logging.FileHandler(f"{uuid.uuid4()}.log", encoding="utf-8")
+log_folder = pathlib.Path(f"{pathlib.Path(__file__).parent}/log")
+
+if not log_folder.exists():
+    os.mkdir(log_folder)
+
+file_handler = logging.FileHandler(f"{log_folder}/{uuid.uuid4()}.log", encoding="utf-8")
 file_handler_formatter = logging.Formatter("[%(asctime)s][%(levelname)s] %(message)s")
 file_handler.setFormatter(file_handler_formatter)
 
